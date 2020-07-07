d47fedd995ff73265eebd9bc3a6bd9537041d23a
2bug <358746595@qq.com>
2020-07-07 09:55:42 +0800

    verify some detail

--- backend/testdj/settings.py
+++ backend/testdj/settings.py
@@ -94,14 +94,14 @@ WSGI_APPLICATION = 'testdj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        # 'ENGINE': 'django.db.backends.mysql',
-        # 'NAME':'Fangzx$TEST',
-        # 'USER':'Fangzx',
-        # 'PASSWORD':'hedyfattoo',
-        # 'HOST':'Fangzx.mysql.pythonanywhere-services.com',
-        # 'PORT':'3306',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'Fangzx$TEST',
+        'USER':'Fangzx',
+        'PASSWORD':'hedyfattoo',
+        'HOST':'Fangzx.mysql.pythonanywhere-services.com',
+        'PORT':'3306',
     }
 }
 
